ec47f44c0c3c73e3e36bea45ff55125aa0ca3807
ShaqDeezle <Shaquil360@gmail.com>
2021-07-24 17:20:38 -0400

    commit

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,6 +1,6 @@
-const functions = "firebase-functions";
+const functions = require('firebase-functions');
 
-const admin = "firebase-admin");
+const admin = require('firebase-admin');
 admin.initializeApp();
 
 const db = admin.firestore();
@@ -25,5 +25,52 @@ exports.removeLike = functions.firestore.document('/posts/{creatorId}/userPosts/
             .doc(context.params.postId)
             .update({
                 likesCount: admin.firestore.FieldValue.increment(-1)
+            })
+    })
+
+
+exports.addFollower = functions.firestore.document('/following/{userId}/userFollowing/{FollowingId}')
+    .onCreate((snap, context) => {
+        return db
+            .collection('users')
+            .doc(context.params.FollowingId)
+            .update({
+                followersCount: admin.firestore.FieldValue.increment(1)
+            }).then(() => {
+                return db
+                    .collection('users')
+                    .doc(context.params.userId)
+                    .update({
+                        followingCount: admin.firestore.FieldValue.increment(1)
+                    })
+            })
+    })
+
+exports.removeFollower = functions.firestore.document('/following/{userId}/userFollowing/{FollowingId}')
+    .onDelete((snap, context) => {
+        return db
+            .collection('users')
+            .doc(context.params.FollowingId)
+            .update({
+                followersCount: admin.firestore.FieldValue.increment(-1)
+            }).then(() => {
+                return db
+                    .collection('users')
+                    .doc(context.params.userId)
+                    .update({
+                        followingCount: admin.firestore.FieldValue.increment(-1)
+                    })
+            })
+    })
+
+exports.addComment = functions.firestore.document('/posts/{creatorId}/userPosts/{postId}/comments/{userId}')
+    .onCreate((snap, context) => {
+        return db
+            .collection("posts")
+            .doc(context.params.creatorId)
+            .collection("userPosts")
+            .doc(context.params.postId)
+            .update({
+                commentsCount: admin.firestore.FieldValue.increment(1)
             })
     })
